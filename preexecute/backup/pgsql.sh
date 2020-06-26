5dfd303fc895739ba9ba43f81a736caa2081cd71
Ali <modaresi.mr@gmail.com>
2020-06-26 16:04:47 +0200

    fix issue

--- preexecute/backup/pgsql.sh
+++ preexecute/backup/pgsql.sh
@@ -8,7 +8,7 @@ mkdir -p $VOLUMERIZE_SOURCE/volumerize-pgsql/
 FULL_BACKUP_QUERY="select datname from pg_database where not datistemplate and datallowconn $EXCLUDE_SCHEMA_ONLY_CLAUSE order by datname;"
 
 for PGDATABASE in `psql -h ${PGHOST} -U "${PGUSERNAME}"  -At -c "$FULL_BACKUP_QUERY" postgres`; do
-  echo "pg_dump -Fp starts " ${MYSQL_DATABASE}
-  pg_dump -U "${PGUSERNAME}"  -h "${PGHOST}" "${PGDATABASE}" | ${VOLUMERIZE_SOURCE}/volumerize-pgsql/dump-${PGDATABASE}.sql || true
+  echo "pg_dump -Fp starts " ${PGDATABASE}
+  pg_dump -U "${PGUSERNAME}"  -h "${PGHOST}" "${PGDATABASE}" > ${VOLUMERIZE_SOURCE}/volumerize-pgsql/dump-${PGDATABASE}.sql || true
   
 done
