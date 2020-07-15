7f8148ff578023410779b07ee055b1b776ebe364
Ali <modaresi.mr@gmail.com>
2020-07-15 18:17:34 +0200

    add full dump

--- preexecute/backup/pgsql.sh
+++ preexecute/backup/pgsql.sh
@@ -7,8 +7,10 @@ mkdir -p $VOLUMERIZE_SOURCE/volumerize-pgsql/
 
 FULL_BACKUP_QUERY="select datname from pg_database where not datistemplate and datallowconn $EXCLUDE_SCHEMA_ONLY_CLAUSE order by datname;"
 
+pg_dumpall -U "${PGUSERNAME}"  -h "${PGHOST}" > ${VOLUMERIZE_SOURCE}/volumerize-pgsql/dump-full.sql 
+echo "full dump finished..."
+
 for PGDATABASE in `psql -h ${PGHOST} -U "${PGUSERNAME}"  -At -c "$FULL_BACKUP_QUERY" postgres`; do
   echo "pg_dump -Fp starts " ${PGDATABASE}
   pg_dump -U "${PGUSERNAME}"  -h "${PGHOST}" "${PGDATABASE}" > ${VOLUMERIZE_SOURCE}/volumerize-pgsql/dump-${PGDATABASE}.sql || true
-  
 done
