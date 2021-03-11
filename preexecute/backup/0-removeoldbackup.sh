b72382024ec81f4f12fcbc18fc4e9349c8bff8e7
Ali <modaresi.mr@gmail.com>
2021-03-11 12:30:06 +0100

    add --force to remove old backupes

--- preexecute/backup/0-removeoldbackup.sh
+++ preexecute/backup/0-removeoldbackup.sh
@@ -1,4 +1,4 @@
 
 echo "remove-all-but-n-full" ${REMOVE_ALL_BUT_N_FULL}
-remove-all-but-n-full ${REMOVE_ALL_BUT_N_FULL}
+remove-all-but-n-full --force ${REMOVE_ALL_BUT_N_FULL}
 echo "remove-all-but-n-full finished"
