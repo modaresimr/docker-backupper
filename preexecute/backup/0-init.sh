b8d19e649a6e67ec7d53549a623d3f0757a2e435
modaresimr <9498182+modaresimr@users.noreply.github.com>
2024-04-21 17:27:25 +0000

    fix: bug

--- preexecute/backup/0-init.sh
+++ preexecute/backup/0-init.sh
@@ -1,7 +1,7 @@
 NAME="${BACKUP_PREFIX}_${BACKUP_NAME}"
 PING_URL=$(curl https://${HC_HOST:-hc-ping.com}/api/v3/checks/?tag=${NAME} --header "X-Api-Key: ${HC_API_KEY}"     |jq -r .checks[0].ping_url)
 
-if [ $PING_URL == "null" || $PING_URL == "" ];then
+if [ "$PING_URL" == "null" ] || [ "$PING_URL" == "" ];then
 payload=$(cat <<EOF
 {
   "name": "${NAME}",
