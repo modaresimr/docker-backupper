df0a3a7b1bd359b3a725957c6243a17323ca9dd1
modaresimr <9498182+modaresimr@users.noreply.github.com>
2024-04-21 17:31:51 +0000

    update

--- postexecute/backup/99-last.sh
+++ postexecute/backup/99-last.sh
@@ -1,4 +1,4 @@
 NAME="${BACKUP_PREFIX}_${BACKUP_NAME}"
-PING_URL=$(curl https://${HC_HOST:-hc-ping.com}/api/v3/checks/?tag=${NAME} --header "X-Api-Key: ${HC_API_KEY}"     |jq -r .checks[0].ping_url)
+PING_URL=$(curl https://${HC_HOST:-hc-ping.com}/api/v3/checks/?tag="${NAME}" --header "X-Api-Key: ${HC_API_KEY}"     |jq -r .checks[0].ping_url)
 
 curl $PING_URL 
--- preexecute/backup/0-init.sh
+++ preexecute/backup/0-init.sh
@@ -1,5 +1,5 @@
 NAME="${BACKUP_PREFIX}_${BACKUP_NAME}"
-PING_URL=$(curl https://${HC_HOST:-hc-ping.com}/api/v3/checks/?tag=${NAME} --header "X-Api-Key: ${HC_API_KEY}"     |jq -r .checks[0].ping_url)
+PING_URL=$(curl https://${HC_HOST:-hc-ping.com}/api/v3/checks/?tag="${NAME}" --header "X-Api-Key: ${HC_API_KEY}"     |jq -r .checks[0].ping_url)
 
 if [ "$PING_URL" == "null" ] || [ "$PING_URL" == "" ];then
 payload=$(cat <<EOF
@@ -17,4 +17,4 @@ EOF
                 --header "X-Api-Key: ${HC_API_KEY}" \
                 --data "$payload" | jq -r '.ping_url')
 fi
-curl https://${PING_URL}/start
+curl ${PING_URL}/start
