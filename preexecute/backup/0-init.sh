71e98e34c8d699d154501d22dc04f0332b172e45
modaresimr <9498182+modaresimr@users.noreply.github.com>
2024-04-21 20:18:11 +0330

    Update 0-init.sh

--- preexecute/backup/0-init.sh
+++ preexecute/backup/0-init.sh
@@ -1,1 +1,8 @@
-wget -q https://${HCHOST:-hc-ping.com}/${HC_KEY}/start -T 10 -t 5 -O /dev/null
+PING_URL=$(curl https://${HC_HOST:-hc-ping.com}/api/v3/checks/?tag=$BACKUP_NAME --header "X-Api-Key: ${HC_API_KEY}"     |jq -r .checks[0].ping_url)
+
+if [ $PING_URL == "null" ];then
+     PING_URL=$(curl https://${HC_HOST:-hc-ping.com}/api/v3/checks/ --header "X-Api-Key: ${HC_API_KEY}" \
+        --data '{"name": "${BACKUP_NAME}", "tags": "${BACKUP_NAME}", "grace": 3600, "schedule": "${VOLUMERIZE_JOBBER_TIME}", "channels": "*" }' \
+        |jq -r .checks[0].ping_url)
+fi
+curl https://${PING_URL}/start
