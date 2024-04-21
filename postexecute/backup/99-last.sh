ccc46b0e4cc64bd54d9902b3b2b2c555e245ea21
modaresimr <9498182+modaresimr@users.noreply.github.com>
2024-04-21 20:19:26 +0330

    Update 99-last.sh

--- postexecute/backup/99-last.sh
+++ postexecute/backup/99-last.sh
@@ -1,1 +1,2 @@
-wget -q https://${HCHOST:-hc-ping.com}/${HC_KEY} -T 10 -t 5 -O /dev/null
+PING_URL=$(curl https://${HC_HOST:-hc-ping.com}/api/v3/checks/?tag=$BACKUP_NAME --header "X-Api-Key: ${HC_API_KEY}"     |jq -r .checks[0].ping_url)
+curl $PING_URL 
